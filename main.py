a72ab0b94026808d39491be66a598ea07a2e757c
ezt <64895594+KreosCode@users.noreply.github.com>
2025-03-10 22:32:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import pygame
+from sys import exit
+from data.state import States
+from data.control import Control
+"""CHANGE IT"""
+from data.states.menu.menu import Menu # should found a way to import state only if needed
+from data.states.game.game import Game
+"""_________"""
+
+pygame.init()
+
+"""MAKE THIS TO BE READEN FROM .ini FILE OR SOME SHIT"""
+# general settings
+settings = {    
+    "size": (1920, 1080),
+    "fps": 60,
+    "flags": pygame.FULLSCREEN
+}
+
+# dict of game states
+state_dict = {
+    "menu": Menu(),
+    "game": Game()
+}
+"""__________________________________________________"""
+
+app = Control(**settings)
+app.setup_states(state_dict, "menu")
+app.main_game_loop()
+
+pygame.quit()
+exit()
