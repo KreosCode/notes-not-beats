1c94274dcfb6d67fcc211553958e8058138c5641
ezztactic <5seryoga6@gmail.com>
2025-03-13 15:21:11 +0300

    updating `sprites` folder and changing note_sprite.py

--- data/states/game/note_sprite.py
+++ data/states/game/note_sprite.py
@@ -1,6 +1,11 @@
 import pygame
 
 class Note(pygame.sprite.Sprite):
-    def __init__(self, type, side, sound_name1, sound_name2 = None):
+    def __init__(self, type, **timings):
         super().__init__()
+        self.__dict__.update(timings)
 
+        if type == "single":
+            self.image = 
+        if type == "slider":
+            pass
